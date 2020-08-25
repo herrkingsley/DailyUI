7114638dc94fdeb40952089840d521589d6e00c4
herrkingsley <jonas.carboo@gmail.com>
2020-08-25 22:01:40 +0200

    Added transform rotation to collaps icon

--- 002/script.js
+++ 002/script.js
@@ -3,6 +3,7 @@ const toggelTab = document.querySelectorAll(".checkout__header");
 toggelTab.forEach(element => {
     element.addEventListener("click", () =>{
         element.nextElementSibling.classList.toggle("collaps");
+        element.firstElementChild.classList.toggle("rotate-90-cw");
     });
 });
 
