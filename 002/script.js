4141a10120d9f0ae2613c8642fa1d82f8ae8b648
herrkingsley <jonas.carboo@gmail.com>
2020-08-26 10:12:42 +0200

    Testing out active class on credit card option

--- 002/script.js
+++ 002/script.js
@@ -6,3 +6,14 @@ toggelTab.forEach(element => {
         element.firstElementChild.classList.toggle("rotate-90-cw");
     });
 });
+
+
+//Add active class to credit card buttons
+var cardBtns = document.querySelectorAll(".active-btns__btn");
+for (var i = 0; i < cardBtns.length; i++) {
+      cardBtns[i].addEventListener("click", function() {
+      var current = document.getElementsByClassName("activeB");
+      current[0].className = current[0].className.replace(" activeB", "");
+      this.className += " activeB";
+      });
+    }
