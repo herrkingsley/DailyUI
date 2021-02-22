8805fd4e33e8a0bd7cb278f303e194e4b47e72ef
herrkingsley <jonas.carboo@gmail.com>
2021-02-22 22:46:37 +0100

    Description added and styled

--- 003/Paralax Delsjön/script3.js
+++ 003/Paralax Delsjön/script3.js
@@ -6,7 +6,7 @@ window.addEventListener('scroll',  function (e) {
     let procentOfShrinkL = 0.7 / 800; // shrink waterlevel by procent on Lagre screens
     let procentOfShrinkM = 0.7 / 1100; // shrink waterlevel by procent on Medium screens
     let procentOfShrinkS = 0.7 / 500; // shrink waterlevel by procent on Small screens
-    
+    console.log(scrolled);
 
 
     // add all the elements that will scroll here
@@ -21,6 +21,7 @@ window.addEventListener('scroll',  function (e) {
     branch.style.marginTop =  - scrolled * 0.2 + 'px';
     if (vw > 1000) {
         title.style.marginTop =  scrolled * 0.6 + 'px';
+        
         
     } else {
         title.style.marginTop =  scrolled * 0.69 + 'px';
@@ -48,7 +49,30 @@ window.addEventListener('scroll',  function (e) {
         title.style.marginTop =  scrolled * 0.69 + 'px';
     }
     
-    
+    function renderDesc() {
+        let desc = document.getElementById('desc');
+        let descP = document.querySelectorAll('#desc > p');
+        desc.style.marginTop =  scrolled * 0.09 + 'px';
+
+        if (vw > 1000 && scrolled > 300 && scrolled < 1000) {    
+            desc.style.transform = "translateX(-50%) scale(1)";
+            descP.forEach(element => {
+                element.style.color = "transparent";
+                element.style.textShadow = "0 0 0px rgba(255, 255, 255, 1)";
+            });
+            
+            
+            
+        } else {
+            // desc.style.transform = "translateX(-50%) scale(.8)";
+            descP.forEach(element => {
+                element.style.color = "transparent";
+                element.style.textShadow = "0 0 10px rgba(255, 255, 255, 0)";
+            });
+        }
+        
+    }
+    renderDesc();
 
     function scrollFade(sStart, sEnd, e, oEnd) // Function takes arguments; when 
     {
@@ -56,9 +80,6 @@ window.addEventListener('scroll',  function (e) {
         let scrollLength = sEnd - sStart;
         let opacityMargin = 1 - oEnd;
         let unitPerScroll = opacityMargin / scrollLength;
-        
-        console.log(newScroll);
-
         e.style.opacity = (1 - (newScroll * unitPerScroll));
 
 
@@ -66,3 +87,10 @@ window.addEventListener('scroll',  function (e) {
 
 
 })
+
+function showCoords(event) {
+    var x = event.clientX;
+    var y = event.clientY;
+    var coords = "X coords: " + x + ", Y coords: " + y;
+    alert(coords);
+}
